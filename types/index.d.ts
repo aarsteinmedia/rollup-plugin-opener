bb684b6419a13427b62a1d36fe74b08b82d0878f
Johan Martin Aarstein <johan@aarstein.media>
2026-09-04 15:04:27 +0200

    stash

--- types/index.d.ts
+++
@@ -1,5 +0,0 @@
-import type { RollupServeOptions } from './types';
-export declare function serve(optionsFromProps?: RollupServeOptions): {
-    generateBundle(): Promise<void>;
-    name: string;
-};
